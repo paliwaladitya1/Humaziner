4ebe1d3abd863a12ce9f6606caf59fe44fa72f04
ADITYA PALIWAL <paliwaladitya1@gmail.com>
2018-10-29 02:17:48 +1100

    commit 2

--- samples/Humanizer.MvcSample/Controllers/HomeController.cs
+++ samples/Humanizer.MvcSample/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace Humanizer.MvcSample.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application for local use";
 
             return View();
         }
